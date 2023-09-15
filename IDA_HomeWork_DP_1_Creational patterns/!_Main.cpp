08b1ef4985984ca1fe0cd3936271fb92033c9d3a
Ишутинов Данил <gladius@inbox.ru>
2023-09-15 16:35:17 +0500

    Add project files.

---
+++ IDA_HomeWork_DP_1_Creational patterns/!_Main.cpp
@@ -0,0 +1,217 @@
+﻿// HomeWork template project v 6.0 // 27.06.2023 //
+
+/*
+
+Наследование - Композиция, Агрегация
+
+Задание на композицию
+
+Для реализованных семейств классов Растений/Плодов/Локаций/Рюкзаков 
+реализовать генерацию новых объектов с помощью пораждающих паттернов:
+
+
+*/
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+#include <conio.h>
+//My own headers
+#include "Service functions.h"
+#include "MainMenu.h"
+#include "Location.h"
+#include "BackPack.h"
+
+
+
+// Homework DP 1 ----------------------------------------------------	
+
+
+//Task 1 Location
+void Task_1(std::string name_of_task)
+{	
+	do
+	{		
+		system("cls");
+		std::cout << "***\t" << name_of_task << "\n";					
+	
+		//создаем локацию с от 30 до 100 рандомными деревьями		
+		TreeLocation Location_1;				
+		
+		//Получим категоризированную сводку по локации
+		std::cout << "\n--- Location_1.Info_categorized():\n";
+		Location_1.Info_categorized();
+		//Это  короткая сводка, вообще еще есть две по степени детализации:
+		//Location_1.Info(); // полная сводка по каждому дереву
+		//Location_1.ShowTrees(); // краткая сводка по каждому дереву
+						
+		
+		// Перебор деревьев
+
+		// Способ 1 - []
+		std::cout << "\n\n--- iterating through the elements []:\n";
+		for (int i = 0; i < Location_1.size(); ++i)
+			std::cout << Location_1[i].Get_Name() << " | ";
+
+		// Способ 2 - iterator
+		std::cout << "\n\n--- iterating through the iterator:\n";
+		for (auto iter = Location_1.begin(); iter != Location_1.end(); iter++)
+			std::cout << (*iter).Get_Name() << " | ";
+		
+		// Способ 2.1 - for-each
+		std::cout << "\n\n--- iterating through the ranged-based for loop:\n";
+		//for (auto iter : Location_1)
+		for (auto& iter : Location_1)
+			std::cout << iter.Get_Name() << " | ";
+
+
+		// Способ 3 - at()
+		std::cout << "\n\n--- iterating through the at():\n";
+			for (int i = 0; i < Location_1.size(); ++i)
+				std::cout << Location_1.at(i).Get_Name() << " | ";
+
+		//Сбор всех плодов в рюкзак
+		std::cout << "\n\n\n--- Harvesting all fruits from all trees in location\n";
+		BackPack temp_back_pack;
+		
+		for (auto iter = Location_1.begin(); iter != Location_1.end(); iter++)
+			while ((*iter).Get_fruits_remain())
+				temp_back_pack << *(*iter).Get_fruit();
+
+		// без ссылки не работает -> хочет экземпляры MotherPlant создавать, а он абстрактный
+		//* 
+		//for (auto iter : Location_1) 
+		for (auto& iter : Location_1) 
+		while (iter.Get_fruits_remain())
+			temp_back_pack << *(iter.Get_fruit());
+		//*/
+		
+		//Заглянем в рюкзак
+		std::cout << "\n\n--- temp_back_pack.ShortInfo():\n";
+		temp_back_pack.ShortInfo();
+		
+		//Осмотрим локацию
+		std::cout << "\n\n--- Location_1.Info_categorized():";
+		Location_1.Info_categorized();	
+			
+		//Удаление всех плодов одного типа
+		std::cout << "\n--- Delete all Pears:\n";
+		temp_back_pack.DeleteItem(Fruit::fruitTypes::Pear);
+				
+		std::cout << "\n\n--- ShortInfo():\n";
+		temp_back_pack.ShortInfo();
+
+		std::cout << "\n\nEsc - exit | any key to continue";
+	} while (_getch() != 27);
+}
+
+//Task 2 BackPack
+void Task_2(std::string name_of_task)
+
+{
+	do
+	{
+		system("cls");
+		std::cout << "***\t" << name_of_task << "\n";
+				
+		BackPack main_back_pack;
+		
+		//Создадим несколько деревьев, чтобы собрать с них фрукты в рюкзак
+		std::vector<MotherPlant*> trees_vec = { new AppleTree(2, Color::white, 50), new Raspberry_bush(1, Color::green, 70), new PearTree(2, Color::brown, 30) };
+		
+		//соберем в рюкзак по 5 разных фруктов
+		std::cout << "\n\n--- Harvesting 5 Apples, 5 Pears, 5 Raspberrys:\n";
+		for ( int tree = trees_vec.size(); tree; --tree)
+			for (int i = 0; i < 5; ++i)		
+				main_back_pack.AddItem(trees_vec[tree-1]->Get_fruit());
+						
+		// Еще есть такой вариант метода положить в рюкзак
+		Fruit& new_fruit = *new Apple(0.2, Color::green);
+		main_back_pack << new_fruit;
+
+		std::cout << "\n\n--- ShortInfo():\n";
+		main_back_pack.ShortInfo();
+		//Перебор элементов Способ 1 []
+		std::cout << "\n\n--- iterating through the elements []:\n";
+		for (int i = 0; i < main_back_pack.size(); ++i)
+		{
+			//std::cout << "\nItem " << i + 1;
+			//main_back_pack[i]->Info();
+			std::cout << i + 1 <<":" << main_back_pack[i]->Get_name() <<" | ";
+		}
+
+		// удаление 1-го элемента
+		std::cout << "\n\n\n--- DeleteItem(1):\n";
+		main_back_pack.DeleteItem(1); 
+		
+		std::cout << "\n\n--- ShortInfo():\n";
+		main_back_pack.ShortInfo();
+		//Перебор элементов Способ 2 at()
+		std::cout << "\n\n--- iterating through the at():\n";
+		for (int i = 0; i < main_back_pack.size(); ++i)
+		{
+			//std::cout << "\nItem " << i + 1;
+			//main_back_pack.at(i).Info();
+			std::cout << i + 1 << ":" << main_back_pack.at(i).Get_name() << " | ";
+
+		}
+				
+		// удаление элементов в диапазоне
+		std::cout << "\n\n\n--- DeleteItem(3,5):\n";
+		main_back_pack.DeleteItem(3,5); 
+		
+		std::cout << "\n\n--- ShortInfo():\n";
+		main_back_pack.ShortInfo();
+		//Перебор элементов Способ 3 iterator
+		std::cout << "\n\n--- iterating through the iterator:\n";
+		int i=0;
+		for (auto iter : main_back_pack)			
+			std::cout << ++i << ":" << iter.Get_name() << " | ";
+
+
+		//Удаление всех плодов одного типа
+		std::cout << "\n\n\n--- Delete all Apples:\n";
+		main_back_pack.DeleteItem(Fruit::fruitTypes::Apple);
+
+		std::cout << "\n\n--- ShortInfo():\n";
+		main_back_pack.ShortInfo();
+
+
+
+		std::cout << "\n\n\nGet ready to watch full BackPack info:";
+		_getch();
+		main_back_pack.Info();
+
+		std::cout << "\n\nEsc - exit | any key to continue";
+	} while (_getch() != 27);
+}
+
+//Task 3 
+void Task_3() {}
+
+//Task 4
+void Task_4() {}
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+	srand(time(NULL));
+	//setlocale(LC_CTYPE, "Russian");
+	//system("mode con cols=60 lines=40"); 
+	Main_menu MainMenu;	
+	MainMenu.AddElement("Design Patterns. Home Work 1: Creational patterns in My_Game_App (Plant, Fruit, BackPack, Loacation class world)");	// Homework name
+	MainMenu.AddElement("Location container class demo");
+	MainMenu.AddElement("BackPack container class demo");
+	MainMenu.AddElement("Fabric objects generation demo");
+
+
+	do 
+	{		
+		MainMenu.Show_menu();
+		if (!MainMenu.User_Choice_Handle(_getch()))	break;
+	} while (true);
+	return 0;
+}
+
---
+++ IDA_HomeWork_DP_1_Creational patterns/BackPack.h
@@ -0,0 +1,200 @@
+﻿#pragma once
+#include "Fruit.h"
+#include <memory>
+#include <vector>
+#include <cassert>
+#include <map>
+
+class BackPack_Player_Interface
+{
+public:
+	virtual ~BackPack_Player_Interface() = default;
+	//virtual void AddItem(const Fruit& new_fruit) = 0;
+	virtual void AddItem(Fruit* new_fruit) = 0;
+	virtual void DeleteItem(size_t) = 0;
+	virtual void Info() = 0;
+	virtual void operator << (const Fruit& new_fruit) = 0;
+	virtual void operator << (Fruit& new_fruit) = 0;
+
+};
+class BackPack_TheGame_Interface
+{
+public:
+	virtual ~BackPack_TheGame_Interface() = default;
+	virtual void Info() = 0;
+};
+
+class BackPack : virtual public BackPack_Player_Interface, virtual public BackPack_TheGame_Interface
+{
+	//std::vector<std::unique_ptr<Fruit>> _items_list;
+	std::vector<Fruit*> _items_list;
+
+	// это закрытая функция, возможности стирать произволную строчку не будет, только через вызов с enum fruitTypes
+	void DeleteItem(std::string type)
+	{
+		//for (auto iter : _items_list)
+		for (int i=0; i< _items_list.size(); ++i)
+		{
+			if (_items_list[i]->Get_name() == type)
+			{
+				delete _items_list[i]; // удаляем объект из кучи, перед удалением указателя из вектора
+				_items_list.erase(_items_list.begin() + i);
+				--i; // изза erase все элементы сдвигаются и надо еще раз проверить i-ый
+			}
+		}
+	}
+public:
+
+	BackPack() {};
+
+	//void AddItem(const Fruit& new_fruit) override
+	void AddItem(Fruit* new_fruit) override
+	{
+	if(new_fruit) _items_list.push_back(new_fruit);
+	//if (!new_fruit) _items_list.emplace_back(new_fruit);
+	}
+	void DeleteItem(size_t index) override
+	{
+		if (index < 0 || index >= _items_list.size()) assert(true && "Deleting index out of range");
+		delete _items_list[index];
+		_items_list.erase(_items_list.begin() + index);		
+	}
+	void DeleteItem(size_t begin_index, size_t end_index)
+	{
+		if (begin_index < 0 || begin_index >= _items_list.size()) assert(true && "Deleting index out of range");
+		if (end_index < 0 || end_index >= _items_list.size()) assert(true && "Deleting index out of range");
+		if (begin_index > end_index) std::swap(begin_index, end_index);
+		
+		for (int i = begin_index; i < end_index; ++i)
+			delete _items_list[i];
+
+		_items_list.erase(_items_list.begin() + begin_index, _items_list.begin() + end_index);
+	}
+	void DeleteItem(Fruit::fruitTypes type)
+	{
+		switch (type)
+		{
+		case Fruit::fruitTypes::Apple:
+			DeleteItem("Apple"); break;
+		case Fruit::fruitTypes::Raspberry:
+			DeleteItem("Raspberry"); break;
+		case Fruit::fruitTypes::Pear:
+			DeleteItem("Pear"); break;
+		}		
+	}
+
+
+
+	void Info() override
+	{
+		if (_items_list.size() == 0)
+		{
+			std::cout << "No items";
+			return;
+		}
+		for (int i = 0; i < _items_list.size(); i++)
+		{
+			std::cout << "\n\nItem " << i + 1;
+			_items_list[i]->Info();
+		}
+	}
+	void ShortInfo()
+	{
+		//определяем какие есть в рюкзаке категории и сколько в каждой категории экземпляров
+		std::map <std::string, int> ingroup_count;
+		std::map <std::string, double> ingroup_weight;
+
+		double total_weight = 0;
+		for (int i = 0; i < _items_list.size(); ++i)
+		{
+			++ingroup_count[_items_list[i]->Get_name()];
+			ingroup_weight[_items_list[i]->Get_name()] += _items_list[i]->Get_weight();
+			total_weight += _items_list[i]->Get_weight();
+		}
+		//выводим категорированную информацию о содержимом рюкзака			
+		for (auto iter = ingroup_count.begin(); iter != ingroup_count.end(); iter++)
+		{
+			std::cout << iter->first << ": " << iter->second << "\t weight: " << ingroup_weight[iter->first] << " kg\n";
+		}
+		std::cout << "\nTotal items  " << _items_list.size();
+		std::cout << "\nTotal weight " << total_weight << " kg";
+
+	}
+
+	 void operator << (const Fruit& new_fruit) override
+	{		 
+		//_items_list.push_back(std::make_unique<Fruit>(std::move(new_fruit)));
+		 assert(true);
+	}
+	 void operator << (Fruit& new_fruit) 
+	 {
+		// _items_list.push_back(std::make_unique<Fruit>(std::move(new_fruit)));
+		// _items_list.push_back(&new_fruit);
+		 AddItem(&new_fruit);
+	 }
+
+	 Fruit* operator [] (int index)
+	 {
+		 if (index < 0 || index >= _items_list.size()) assert(true && "Index out of range");
+		 return _items_list[index];
+	 }
+	 size_t size() { return _items_list.size(); }
+	 auto at(size_t index) const -> Fruit&
+	 {
+		 return *_items_list.at(index);
+	 }
+
+	 class iterator
+	 {
+		 Fruit** _data_iterator; // в предположении, что в итераторе должен быть тип указателя на тип основного класса
+
+	 public:
+		 iterator() = delete;
+		 iterator(Fruit** data) : _data_iterator(data) {}
+
+		 iterator& operator++()
+		 {
+			 _data_iterator++;
+			 return *this;
+		 }
+
+		 iterator& operator++(int)
+		 {
+			 iterator tmp(*this);
+			 _data_iterator++;
+			 return tmp;
+		 }
+
+		 iterator& operator--()
+		 {
+			 _data_iterator--;
+			 return *this;
+		 }
+
+		 iterator& operator--(int)
+		 {
+			 iterator tmp(*this);
+			 _data_iterator--;
+			 return tmp;
+		 }
+
+		 Fruit& operator*()
+		 {
+			 //return *_data_iterator;
+			 return **_data_iterator;
+		 }
+		 bool operator != (const iterator& obj)
+		 {
+			 return (this->_data_iterator != obj._data_iterator);
+		 }
+		 bool operator == (const iterator& obj)
+		 {
+			 return this->_data_iterator == obj._data_iterator;
+		 }
+
+	 };
+	 iterator begin() { return &_items_list[0]; }
+	 iterator end() { return &_items_list[0] + _items_list.size(); }
+
+
+};
